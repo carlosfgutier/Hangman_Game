6e59238880f0a6aab45bd928a69785eb9bd3318c
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-11-11 15:04:17 -0600

    stuck at comparing event key to letter in array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,85 +7,61 @@ var blanks = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "
 //array of possible words to guess
 var pieces = ["The Trinquetaille Bridge", "The Yellow House", "Four Cut Sunflowers", "Noon Rest From Work"];
 
+//status variables
+var wins = 0;
+var guessesLeft = 10;
+var guessesMade = [];
+var wordToGuess = "";
+var piecePicked;
 
-document.getElementById("start").textContent = "Press Enter to begin.";
+//PC picks a piece
+var index = Math.floor(Math.random() * pieces.length);
+var piecePicked = pieces[index];
 
-//run following function when Enter is pressed
+//PC pick becomes uppercased
+var piecePickedUp = piecePicked.toUpperCase();
+console.log(piecePickedUp);
+
+
+//PC pick becomes one array
+var arrWord = piecePickedUp.split("");
+console.log(arrWord);
+
+//Display blanks instead of letters
+for (i = 0; i < arrWord.length; i++) {
+	wordToGuess += blanks[i] + " ";
+}
+
+document.getElementById("start").textContent = "Press 'Enter' to begin.";
+document.getElementById("wins").textContent = "Wins";
+document.getElementById("guessesLeft").textContent = "Guesses remaining";
+document.getElementById("guessesMade").textContent = "Guesses made so far";
+
+
+//When Enter is pressed, status are set to default
 document.onkeyup = function(event) {
-	if (event.keyCode = 13) {
-		
-		//Definition of status variables
-		var wins = 0;
-		var guessesLeft = 10;
-		var guessesMade = [];
-		var wordToGuess = "";
-		var piecePicked;
-		
-		//PC picks a piece
-		var index = Math.floor(Math.random() * pieces.length);
-		var piecePicked = pieces[index];
-		
-		//PC pick becomes uppercased
-		var piecePickedUp = piecePicked.toUpperCase();
-		console.log(piecePickedUp);
-		
-		//PC pick becomes one array
-		var arrWord = piecePickedUp.split("");
-		console.log(arrWord);
-		
-		//display blanks instead of letters
-		for (i = 0; i < arrWord.length; i++) {
-			wordToGuess += blanks[i] + " ";
+	var upperKey = event.key.toUpperCase();
+
+	document.getElementById("start").textContent = "Name of piece";
+	document.getElementById("word").textContent = wordToGuess;
+	console.log(wordToGuess);
+
+	if (guessesMade.indexOf(upperKey) < 0 && event.keyCode >= 65 && event.keyCode <= 90){
+
+		for (var i =0; i < arrWord.length; i++) {
+			// check if key pressed is present in the name of the piece
+			if (event.keyCode === arrWord[i].keyCode) {
+				var present = true;	
+			} else {
+				present = false;
+			}
 		}
-		document.getElementById("word").textContent = wordToGuess;
-		document.getElementById("start").textContent = "Name of piece";
-		document.getElementById("wins").textContent = "Wins";
-		document.getElementById("guessesLeft").textContent = "Guesses remaining";
-		document.getElementById("guessesMade").textContent = "Gueses made so far";
+		//if key pressed is pressent in word the edit word to guess array to show event key in correct spot
+		if (present = true) {
+			console.log(event.key.toUpperCase())
+			// wordToGuess.splice(arrWord.indexOf(arrWord[i], arrWord.indexOf(arrWord[i], event.key));
+		}
 	}
 };
 
-// var arrWord = piecePickedUp.split("");
-// console.log(arrWord);
-// document.getElementById("word").textContent = arrWord;
 
-
-// //When run, code will reset all player status but wins
-// function reset() {
-// 	guessesLeft = 10;
-// 	guessesMade = [];
-// 	index = Math.floor(Math.random() * pieces.length);
-// 	piecePicked = options[index];
-// }
-
-
-
-// document.onkeyup = function(event) {
-// 	//Uppercase keypressed
-// 	var keyPressed = event.key.toUpperCase();
-// 	console.log(keyPressed);
-
-
-// }
-
-// document.getElementById("wins").textContent = "Wins " + wins;
-
-// document.getElementById("guessesLeft").textContent = "Guesses remaining " + guessesLeft;
-
-// document.getElementById("guessesMade").textContent = "Guesses made so far " + guessesMade;
-
-// document.getElementById("instructions").textContent = "";
-
-//PRESS ANY KEY TO GET STARTED
-
-//WINS
-
-//CURRENT WORD
-	//letter shows when right letter key pressed
-
-//GUESSES REMAINING
-	//guesses remaining go down when letter key pressed
-	//if letter already typed, do nothing
-
-//LETTERS ALREADY GUESSED
-	//show letter when key pressed
