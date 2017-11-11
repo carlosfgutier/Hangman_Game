7d1e829ef2e543c71b05920c96eff02d5a9266b7
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-11-11 00:48:49 -0600

    progress has been made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,80 @@
+//alphabet
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z'"];
 
+//array of blanks
+var blanks = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"];
+
+//array of possible words to guess
 var pieces = ["The Trinquetaille Bridge", "The Yellow House", "Four Cut Sunflowers", "Noon Rest From Work"];
 
-//Player status
-var wins = 0;
-var guessesLeft = 10;
-var guessesMade = [];
-var piecePicked;
 
-//PC picks piece
-var index = Math.floor(Math.random() * pieces.length);
-var piecePicked = pieces[index];
-console.log(piecePicked);
+document.getElementById("start").textContent = "Press Enter to begin.";
 
-//When run, code will reset all player status but wins
-function reset() {
-	guessesLeft = 10;
-	guessesMade = [];
-	index = Math.floor(Math.random() * pieces.length);
-	piecePicked = options[index];
-}
+//run following function when Enter is pressed
+document.onkeyup = function(event) {
+	if (event.keyCode = 13) {
+		
+		//Definition of status variables
+		var wins = 0;
+		var guessesLeft = 10;
+		var guessesMade = [];
+		var wordToGuess = "";
+		var piecePicked;
+		
+		//PC picks a piece
+		var index = Math.floor(Math.random() * pieces.length);
+		var piecePicked = pieces[index];
+		
+		//PC pick becomes uppercased
+		var piecePickedUp = piecePicked.toUpperCase();
+		console.log(piecePickedUp);
+		
+		//PC pick becomes one array
+		var arrWord = piecePickedUp.split("");
+		console.log(arrWord);
+		
+		//display blanks instead of letters
+		for (i = 0; i < arrWord.length; i++) {
+			wordToGuess += blanks[i] + " ";
+		}
+		document.getElementById("word").textContent = wordToGuess;
+		document.getElementById("start").textContent = "Name of piece";
+		document.getElementById("wins").textContent = "Wins";
+		document.getElementById("guessesLeft").textContent = "Guesses remaining";
+		document.getElementById("guessesMade").textContent = "Gueses made so far";
+	}
+};
+
+// var arrWord = piecePickedUp.split("");
+// console.log(arrWord);
+// document.getElementById("word").textContent = arrWord;
 
 
-document.getElementById("wins").textContent = "Wins " + wins;
+// //When run, code will reset all player status but wins
+// function reset() {
+// 	guessesLeft = 10;
+// 	guessesMade = [];
+// 	index = Math.floor(Math.random() * pieces.length);
+// 	piecePicked = options[index];
+// }
 
-document.getElementById("guessesLeft").textContent = "Guesses remaining " + guessesLeft;
 
-document.getElementById("guessesMade").textContent = "Guesses made so far " + guessesMade;
 
+// document.onkeyup = function(event) {
+// 	//Uppercase keypressed
+// 	var keyPressed = event.key.toUpperCase();
+// 	console.log(keyPressed);
+
+
+// }
+
+// document.getElementById("wins").textContent = "Wins " + wins;
+
+// document.getElementById("guessesLeft").textContent = "Guesses remaining " + guessesLeft;
+
+// document.getElementById("guessesMade").textContent = "Guesses made so far " + guessesMade;
+
+// document.getElementById("instructions").textContent = "";
 
 //PRESS ANY KEY TO GET STARTED
 
