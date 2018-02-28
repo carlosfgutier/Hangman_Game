98cad84741172338a4f44d71a2fc1259b3d06651
Carlos <carlosfgutier@gmail.com>
2018-02-28 17:18:35 -0600

    generate html and computer guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,193 +1,84 @@
-//alphabet
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z'"];
+let word = "";
+let blanks = [];
+let wins = 0;
+let guessesLeft = 9;
+let correct = [];
+let all = [];
 
-//array of blanks
-var blanks = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"];
-
-//array of possible words to guess
-var pieces = ["The Trinquetaille Bridge", "The Yellow House", "Four Cut Sunflowers", "Noon Rest From Work"];
-
-//status variables
-var wins = 0;
-var guessesLeft = 10;
-var guessesMade = [];
-var wordToGuess = [];
-var piecePicked;
-
-//PC picks a piece
-var index = Math.floor(Math.random() * pieces.length);
-
-//Uppercase pc pick
-piecePicked = pieces[index].toUpperCase();
-console.log("Uppercase title of piece picked: " + piecePicked);
-
-//PC pick becomes one array
-var arrWord = piecePicked.split("");
-console.log("Array of letters in piece picked: " + arrWord);
-
-//Create array of blanks that is just as long as the array containing letters of pc pick
-for (i = 0; i < arrWord.length; i++) {
-	wordToGuess += blanks[i] + " ";
+// Pc chooses
+const choices = ["The Trinquetaille Bridge", "The Yellow House", "Four Cut Sunflowers", "Noon Rest From Work"];
+let pick = Math.floor(Math.random() * choices.length)
+let pickArr = choices[pick].toUpperCase().split("");
+console.log(pickArr);
+for (var i = 0; i < pickArr.length; i++) {
+	blanks.push("_ ")
 }
 
-//display array of blanks, which is the word to guess, without the comas
-var arrBlanks = wordToGuess.split("");
-console.log("Array of blanks" + arrBlanks);
-
-document.getElementById("start").textContent = "Name of piece";
-document.getElementById("word").textContent = arrBlanks.join("");
-document.getElementById("wins").textContent = "Wins";
-document.getElementById("guessesLeft").textContent = "Guesses remaining";
-document.getElementById("guessesMade").textContent = "Guesses made so far";
-
-document.onkeyup = function(event) {
-	//keuUp becomes capitalized verstion of event key
-	var keyUp = event.key.toUpperCase();
-	console.log(keyUp);
-
-
-	for (var i = 0; i < arrWord.length; i++) {
-		if (arrWord[i].includes(keyUp)) {
-			arrBlanks[i] = keyUp;
-			
-		}	
-	}
-	document.getElementById("word").textContent = arrBlanks.join("");
-	console.log(arrBlanks);
-};
-
-// for (var i = 0; i < wordGuessDisplay.length; i++) {
-// 	if (wordGuessDisplay[i] != " ") {
-// 		wordGuessDisplay[i] = "_";
-// 	}	
-// }
-
-	// //if event key had not been pressed before, run loop
-	// if (event.keyCode >= 65 && event.keyCode <= 90) {
-	// 	for (var i = 0; i < arrWord.length; i++) {
-	// 		//if the code of the event key matches the code of a letter arrWord
-			
-	// 	}
-			
-	// }
-
-
-// console.log("Array to be displayed on html: " + wordToGuess);
-
-// document.getElementById("start").textContent = "Press 'Enter' to begin.";
-
-// document.getElementById("start").textContent = "Name of piece";
-// document.getElementById("word").textContent = wordToGuess;
-// document.getElementById("wins").textContent = "Wins";
-// document.getElementById("guessesLeft").textContent = "Guesses remaining";
-// document.getElementById("guessesMade").textContent = "Guesses made so far";
+// Lay out html
+$( document ).ready(function() {
+	$('#word').text(blanks.join(''));
+	$('#wins').text('Wins: ' + wins);
+	$('#guessesLeft').text('Guesses Left: ' + guessesLeft);
+	$('#correctGuesses').text('Correct Guesses: ' + correct);
+	$('#allGuesses').text('All Guesses: ' + all);
+});
 
 
 
+// //alphabet
+// var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z'"];
 
+// //array of blanks
+// var blanks = ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"];
 
+// //array of possible words to guess
+// var pieces = ["The Trinquetaille Bridge", "The Yellow House", "Four Cut Sunflowers", "Noon Rest From Work"];
 
+// //status variables
+// var wins = 0;
+// var guessesLeft = 10;
+// var guessesMade = [];
+// var wordToGuess = [];
+// var piecePicked;
 
+// //PC picks a piece
+// var index = Math.floor(Math.random() * pieces.length);
 
-
-// document.onkeyup = function(event) {
-// 	//keyUp becomes capitalized version of event key
-// 	var keyUp = event.key.toUpperCase();
-// 	//If event key is a letter key that has not been pressed run loop
-// 	if (guessesMade.indexOf(keyUp) < 0 && keyUp.keyCode >= 65 && keyUp.keyCode <=90) {
-// 		for (var i = 0; i < arrWord.length; i++) {
-// 			//if the code of the event key matches the code of a letter in the array, 
-// 			if (keyUp.keyCode === arrWord[i].keyCode) {
-// 				arrWord[i] = arrWord[i].replace('arrWord[i]', 'keyUp')
-// 			}
-
-			
-// 	}
-
-	
-// 	}
-// };
-
+// //Uppercase pc pick
+// piecePicked = pieces[index].toUpperCase();
+// console.log("Uppercase title of piece picked: " + piecePicked);
 
 // //PC pick becomes one array
-// var arrWord = piecePickedUp.split("");
-// console.log(arrWord);
+// var arrWord = piecePicked.split("");
+// console.log("Array of letters in piece picked: " + arrWord);
 
-// //Display blanks instead of letters
+// //Create array of blanks that is just as long as the array containing letters of pc pick
 // for (i = 0; i < arrWord.length; i++) {
 // 	wordToGuess += blanks[i] + " ";
 // }
 
+// //display array of blanks, which is the word to guess, without the comas
 // var arrBlanks = wordToGuess.split("");
-// console.log(arrBlanks);
+// console.log("Array of blanks" + arrBlanks);
 
+// document.getElementById("start").textContent = "Name of piece";
+// document.getElementById("word").textContent = arrBlanks.join("");
+// document.getElementById("wins").textContent = "Wins";
+// document.getElementById("guessesLeft").textContent = "Guesses remaining";
+// document.getElementById("guessesMade").textContent = "Guesses made so far";
 
-
-//When Enter is pressed, status are set to default
 // document.onkeyup = function(event) {
-// 	var upperKey = event.key.toUpperCase();
+// 	//keuUp becomes capitalized verstion of event key
+// 	var keyUp = event.key.toUpperCase();
+// 	console.log(keyUp);
 
-// 	document.getElementById("start").textContent = "Name of piece";
-// 	document.getElementById("word").textContent = wordToGuess;
-// 	console.log(wordToGuess);
 
-// 	if (guessesMade.indexOf(upperKey) < 0 && event.keyCode >= 65 && event.keyCode <= 90){
-// 		for (var i = 0; i < arrWord.length; i++) {
-// 			if (event.keyCode === arrWord.keyCode) {
-// 				if (event.keyCode === arrWord[i].keyCode) {
-// 					var present = true;
-// 					if (present === true) {
-// 						for (var i = 0; i < arrWord.length; i++) {
-// 							arrBlanks[i] === arrBlanks[i].replace('arrBlanks[i]', 'upperKey');
-// 							console.log(arrBlanks);
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}
-
-		
+// 	for (var i = 0; i < arrWord.length; i++) {
+// 		if (arrWord[i].includes(keyUp)) {
+// 			arrBlanks[i] = keyUp;
+			
+// 		}	
 // 	}
-// };	
-
-// 		for (var i =0; i < arrWord.length; i++) {
-// 			// check if key pressed is present in the name of the piece
-// 			if (event.keyCode === arrWord[i].keyCode) {
-// 				var present = true;	
-// 			} else {
-// 				present = false;
-// 			}
-// 		}
-// 		//if key pressed is pressent in word the edit word to guess array to show event key in correct spot
-// 		if (present = true) {
-// 			console.log(event.key.toUpperCase())
-// 			// wordToGuess.splice(arrWord.indexOf(arrWord[i], arrWord.indexOf(arrWord[i], event.key));
-// 		}
-// 	}
+// 	document.getElementById("word").textContent = arrBlanks.join("");
+// 	console.log(arrBlanks);
 // };
-
-// //If key pressed has not been pressed, let conditional run. If key pressed has been pressed, do nothing.
-// 	if (guessesMade.indexOf(lowerCaseLetter) < 0 && event.keyCode >= 65 && event.keyCode <= 90){
-
-// 		//if player guesses PCpick, player wins and game restarts
-// 		if (lowerCaseLetter === PCpick) {	
-// 			wins++;
-// 			alert("I guess you are a psychic");
-// 			reset();
-// 		}
-
-// 		//if player guesses wrong and has no guesses left, player loses and game restarts
-// 		else {
-// 			guessesLeft--;
-// 			if (guessesLeft === 0) {
-// 				losses++;
-// 				reset();
-// 				alert("You're probably not psychic, but feel free to try again");
-// 			}
-// 			//if player guesses wrong and still has guesses left, player's guesses left drop by one and letter is documented
-// 			else{
-// 				guessesLeft - 1;
-// 				guessesMade.push(lowerCaseLetter);
-// 			}
-// 		}
-// 	}
